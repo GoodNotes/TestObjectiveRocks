079a73c8f67f00ee8c169fddf84d6a8039488090
James Tang <james@goodnotesapp.com>
2018-12-17 18:31:20 +0800

    Initial Commit

---
+++ TestObjectiveRocks/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TestObjectiveRocks
+//
+//  Created by James Tang on 17/12/2018.
+//  Copyright © 2018 Time Base Technology Limited. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
